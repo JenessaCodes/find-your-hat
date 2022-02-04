56b5116dbedc248aa543c1d6b65a26026f915fe4
Jenessa Codes <87744322+JenessaCodes@users.noreply.github.com>
2022-02-04 13:38:38 -0700

    Make printField function

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(fieldArray){
+    this._fieldArray=fieldArray;
+  }
+
+  printField(){
+    for (let i=0;i<this._fieldArray.length;i++)
+    console.log(this._fieldArray[i].join(''));
+  }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+]);
+
+  myField.printField()
